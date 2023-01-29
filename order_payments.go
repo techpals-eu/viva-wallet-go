0371f39b460a2dcdb8af3a96200e7109c363fe6c
ipinak <ipinak@localhost>
2023-01-29 19:07:01 +0200

    (#1): Create order payments and authenticate with OAuth2

---
+++ order_payments.go
@@ -0,0 +1,85 @@
+package vivawallet
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type CheckoutOrderRequest struct {
+	Amount               int64  `json:"amount"`
+	CustomerTransactions string `json:"customerTrns"`
+	Customer             struct {
+		Email       string `json:"email,omitempty"`
+		FullName    string `json:"fullName,omitempty"`
+		Phone       string `json:"phone,omitempty"`
+		CountryCode string `json:"countryCode,omitempty"`
+		RequestLang string `json:"requestLang,omitempty"`
+	} `json:"customer,omitempty"`
+	PaymentTimeout       int      `json:"paymentTimeout,omitempty"`
+	AllowRecurring       bool     `json:"allowRecurring,omitempty"`
+	MaxInstallments      int      `json:"maxInstallments,omitempty"`
+	PaymentNotification  bool     `json:"paymentNotification,omitempty"`
+	TipAmount            int64    `json:"tipAmount,omitempty"`
+	DisableExactAmount   bool     `json:"disableExactAmount,omitempty"`
+	DisableCash          bool     `json:"disableCash,omitempty"`
+	DisableWallet        bool     `json:"disableWallet,omitempty"`
+	SourceCode           string   `json:"sourceCode,omitempty"`
+	MerchantTransactions string   `json:"merchantTrns,omitempty"`
+	Tags                 []string `json:"tags,omitempty"`
+	CardTokens           []string `json:"cardTokens,omitempty"`
+}
+
+type CheckoutResponse struct {
+	OrderCode int64 `json:"orderCode"`
+}
+
+// CreateOrderPayment creates a new order payment and returns the `orderCode`.
+func (c Client) CreateOrderPayment(order CheckoutOrderRequest) (*CheckoutResponse, error) {
+	uri := checkoutEndpoint(c.Config)
+	data, err := json.Marshal(order)
+	if err != nil {
+		fmt.Errorf("failed to parse order %s", err)
+		return nil, err
+	}
+
+	// Check if auth expired and if so authenticate again
+	if c.HasAuthExpired() {
+		_, authErr := c.Authenticate()
+		fmt.Errorf("authentication error %s", authErr)
+		return nil, authErr
+	}
+
+	req, _ := http.NewRequest("POST", uri, bytes.NewReader(data))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.AuthToken()))
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, httpErr := c.HTTPClient.Do(req)
+	if httpErr != nil {
+		fmt.Errorf("failed to parse order %s", httpErr)
+		return nil, httpErr
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(fmt.Sprintf("failed to make order %d", resp.StatusCode))
+	}
+
+	body, bodyErr := io.ReadAll(resp.Body)
+	if bodyErr != nil {
+		return nil, bodyErr
+	}
+
+	response := &CheckoutResponse{}
+	if jsonErr := json.Unmarshal(body, response); jsonErr != nil {
+		return nil, jsonErr
+	}
+
+	return response, nil
+}
+
+func checkoutEndpoint(c Config) string {
+	return fmt.Sprintf("%s/%s", ApiUri(c), "checkout/v2/orders")
+}
