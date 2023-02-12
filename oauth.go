291ebdd022d8724ac2ee133ba1bf524eb697156b
ipinak <ipinak@localhost>
2023-02-12 18:57:59 +0200

    (#17): rename of httpClient

--- oauth.go
+++ oauth.go
@@ -2,11 +2,14 @@ package vivawallet
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"sync"
+	"time"
 )
 
 // New creates a new viva client for the oauth apis
@@ -17,7 +20,7 @@ func NewOAuth(clientID string, clientSecret string, demo bool) *OAuthClient {
 			ClientID:     clientID,
 			ClientSecret: clientSecret,
 		},
-		HTTPClient: httpClient,
+		Client:     httpClient,
 		tokenValue: &token{},
 		lock:       &sync.RWMutex{},
 	}
@@ -46,7 +49,7 @@ func (c OAuthClient) performReq(req *http.Request, v interface{}) error {
 	req.Header.Add("Content-Type", "application/json")
 	c.setBearerToken(req)
 
-	resp, httpErr := c.HTTPClient.Do(req)
+	resp, httpErr := c.Client.Do(req)
 	if httpErr != nil {
 		return fmt.Errorf("failed to perform request %s", httpErr)
 	}
@@ -65,4 +68,102 @@ func (c OAuthClient) performReq(req *http.Request, v interface{}) error {
 	}
 
 	return json.Unmarshal(body, v)
+}
+
+type TokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int64  `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+	Scope       string `json:"scope"`
+}
+
+// Authenticate retrieves the access token to continue making requests to Viva's API. It
+// returns the full response of the API and stores the token and expiration time for
+// later use.
+func (c OAuthClient) Authenticate() (*TokenResponse, error) {
+	uri := c.tokenEndpoint()
+
+	grant := []byte("grant_type=client_credentials")
+	req, _ := http.NewRequest("POST", uri, bytes.NewBuffer(grant))
+	req.SetBasicAuth(c.Config.ClientID, c.Config.ClientSecret)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, httpErr := c.Client.Do(req)
+	if httpErr != nil {
+		return nil, fmt.Errorf("failed to perform access token request %s", httpErr)
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("non successful response")
+	}
+
+	defer resp.Body.Close()
+	body, bodyErr := io.ReadAll(resp.Body)
+	if bodyErr != nil {
+		return nil, bodyErr
+	}
+
+	response := &TokenResponse{}
+	if jsonErr := json.Unmarshal(body, response); jsonErr != nil {
+		return nil, jsonErr
+	}
+
+	expiry := time.Now().Add(time.Second * time.Duration(response.ExpiresIn))
+	c.SetToken(response.AccessToken, expiry)
+
+	return response, nil
+}
+
+// AuthToken returns the token value
+func (c OAuthClient) AuthToken() string {
+	c.lock.RLock()
+
+	t := c.tokenValue.value
+
+	c.lock.RUnlock()
+	return t
+}
+
+// SetToken sets the token value and the expiration time of the token.
+func (c OAuthClient) SetToken(value string, expires time.Time) {
+	c.lock.Lock()
+
+	c.tokenValue.value = value
+	c.tokenValue.expires = expires
+
+	c.lock.Unlock()
+}
+
+// HasAuthExpired returns true if the expiry time of the token has passed and false
+// otherwise.
+func (c OAuthClient) HasAuthExpired() bool {
+	c.lock.RLock()
+
+	expires := c.tokenValue.expires
+
+	c.lock.RUnlock()
+
+	now := time.Now()
+	return now.After(expires)
+}
+
+func AuthBody(c Config) string {
+	auth := fmt.Sprintf("%s:%s", c.ClientID, c.ClientSecret)
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func BasicAuth(c Config) string {
+	auth := fmt.Sprintf("%s:%s", c.MerchantID, c.APIKey)
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func (c OAuthClient) tokenEndpoint() string {
+	return fmt.Sprintf("%s/%s", c.authUri(), "/connect/token")
+}
+
+func (c OAuthClient) authUri() string {
+	if isDemo(c.Config) {
+		return "https://demo-accounts.vivapayments.com"
+	}
+	return "https://accounts.vivapayments.com"
 }
